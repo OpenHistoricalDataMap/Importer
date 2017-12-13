7b3defc4c4ca58fd049617c62ce15c507c8ca439
NabeelSMADI <smady91@hotmail.com>
2017-12-13 12:19:46 +0100

    2

--- src/java/Import.java
+++ src/java/Import.java
@@ -2,62 +2,41 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 /**
  *
  * @author Smady91
  */
 @WebServlet("/Import")
-@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10, // 10 MB 
-        maxFileSize = 1024 * 1024 * 50, // 50 MB
-        maxRequestSize = 1024 * 1024 * 100)   	// 100 MB
 public class Import extends HttpServlet {
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        String type = request.getParameter("Type");
-        String objectID = request.getParameter("ObjectID");
+        String ObjectName = request.getParameter("ObjectName");
+        String UserID = request.getParameter("UserID");
         String validSince = request.getParameter("Valid-Since");
         String validUntil = request.getParameter("Valid-Until");
-        String URL = request.getParameter("URL");
-         String JSON = request.getParameter("JSON");
-
-        System.out.println(type);
-        System.out.println(objectID);
-        System.out.println(validSince);
-        System.out.println(validUntil);
-        if (type.equals("url")) {
-            System.out.println(URL);
-        } else if (type.equals("Content")) {
-            for (Part part : request.getParts()) {
-                part.write(objectID);
-            }
-        } else {
-         System.out.println(JSON);
-        }
+        String OSM_Feature_Name = request.getParameter("OSM_Feature_Name");
+        String OSM_Feature_Value = request.getParameter("OSM_Feature_Value");
+        String JSON = request.getParameter("JSON");
 
         out.print("Successfully Imported...");
-        out.print("<br/>Type: " + type);
-        out.print("<br/>ObjectID: " + objectID);
+        out.print("<br/>ObjectName: " + ObjectName);
+        out.print("<br/>UserID: " + UserID);
         out.print("<br/>Valid Since: " + validSince);
         out.print("<br/>Valid Until: " + validUntil);
-        if (type.equals("url")) {
-            out.print("<br/>URL: " + URL);
-        }
-        if (type.equals("Geometry")) {
-            out.print("<br/>JSON: " + JSON);
-        }
+        out.print("<br/>OSM_Feature_Name: " + OSM_Feature_Name);
+        out.print("<br/>OSM_Feature_Value: " + OSM_Feature_Value);
+        out.print("<br/>JSON: " + JSON);
         out.close();
 
     }
