6ff2ce9f94055a266e6ce8828eaaaf4fc07a2cd0
NabeelSMADI <smady91@hotmail.com>
2018-01-03 10:20:08 +0100

    03-01-2018

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -16,8 +16,8 @@ public class test {
     public static void main(String[] args) throws SQLException {
         GisConn gisconn = new GisConn();
         gisconn.setConn();
-        int pointID = gisconn.addPoint("ST_GeomFromText('POINT(-71.060316 48.432044)')", "401");
-        System.out.println(pointID);
+      //  int pointID = gisconn.addPoint("ST_GeomFromText('POINT(-71.060316 48.432044)')", "401");
+       // System.out.println(pointID);
     }
 
 }
