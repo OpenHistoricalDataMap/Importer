f94638d1019fba0058978e0832a1abac7e67a6ff
NabeelSMADI <smady91@hotmail.com>
2018-01-18 14:40:52 +0100

    18-01-2018

--- src/main/java/Import.java
+++ src/main/java/Import.java
@@ -24,12 +24,6 @@ public class Import extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        String ObjectName = request.getParameter("ObjectName");
-        String UserID = request.getParameter("UserID");
-        String validSince = request.getParameter("Valid-Since");
-        String validUntil = request.getParameter("Valid-Until");
-        String OSM_Feature_Name = request.getParameter("OSM_Feature_Name");
-        String OSM_Feature_Value = request.getParameter("OSM_Feature_Value");
         String JSON = request.getParameter("JSON");
 
         JSONController jSONController = new JSONController();
@@ -43,15 +37,6 @@ public class Import extends HttpServlet {
             Logger.getLogger(Import.class.getName()).log(Level.SEVERE, null, ex);
             out.print("Error ..." + ex);
         }
-        /*
-        out.print("Successfully Imported...");
-        out.print("<br/>ObjectName: " + ObjectName);
-        out.print("<br/>UserID: " + UserID);
-        out.print("<br/>Valid Since: " + validSince);
-        out.print("<br/>Valid Until: " + validUntil);
-        out.print("<br/>OSM_Feature_Name: " + OSM_Feature_Name);
-        out.print("<br/>OSM_Feature_Value: " + OSM_Feature_Value);
-        out.print("<br/>JSON: " + JSON);*/
         out.close();
 
     }
